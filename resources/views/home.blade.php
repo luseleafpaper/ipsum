1961c02026a5a5d3f918aba5e18679962a25febf
Lu Wang <lu.irena.wang@gmail.com>
2016-11-05 21:16:34 -0400

    added bootstrap and homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title') 
+    Index  
+@stop 
+
+@section('content') 
+    <h1> Developer's Best Friend </h1> 
+    <p> For when you need some filler content </p> 
+
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,21 +6,42 @@
         @yield('title','Lorem Ipsum')
     </title>
 
+    <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' rel='stylesheet'>
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/lumen/bootstrap.min.css' rel='stylesheet'>
+    <link href='/css/ipsum.css' type='text/css' rel='stylesheet'>
+
+
     {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
     @yield('head')
 
 </head>
 <body>
 
-    <header>
-    </header>
-
-    <nav>
-        <ul>
-            <li><a href='/ipsum'>Lorem Ipsum Generator</a></li>
-            <li><a href='/usergen'>User Generator</a></li>
-        </ul>
-    </nav>
+  <div class="container"> 
+    <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Home</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="/ipsum">Lorem Ipsum</a></li>
+              <li><a href="/usergen">Generate Users</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">&copy;{{date('Y')}} Lu Wang</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
 
 
     <section>
@@ -28,10 +49,7 @@
         @yield('content')
     </section>
 
-
-    <footer>
-        <!-- &copy; {{ date('Y') }} &nbsp;&nbsp; --> 
-    </footer>
+  </div>
 
     {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
     @yield('body')
