9f23ee0ab7941db426326650fbc6c54413ddfe88
Lu Wang <lu.irena.wang@gmail.com>
2016-11-05 21:28:35 -0400

    added descriptions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,14 @@
 
 @section('content') 
     <h1> Developer's Best Friend </h1> 
-    <p> For when you need some filler content </p> 
+    
+    <h2><a href="/ipsum">Lorem Ipsum Generator</a></h2>
+    <blockquote>
+    In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the graphic elements of a document or visual presentation. 
+    By replacing the distraction of meaningful content with filler text of scrambled Latin it allows viewers to focus on graphical elements such as font, typography, and layout.
+    </blockquote>
 
+
+    <h2><a href="/usergen">Random User Generator</a></h2>
+    <p>Create random user data for your applications. Like Lorem Ipsum, but for people.</p>
 @endsection
